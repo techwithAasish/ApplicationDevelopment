df59fcf362d48dd3b89ad35723ffa0653182a82d
techwithAasish <aasish.mahato101@gmail.com>
2022-03-28 05:45:35 +0545

    initial commit

---
+++ CrudOperation/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudOperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperation.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options) 
+        {
+        } 
+        
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CrudOperation/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudOperation.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public int Age { get; set; }
+
+    }
+}
